35708f3fd31ee6f7a4dc5f5675b30fc3d09749f6
gibeom01 <rlqja7638@gmail.com>
2024-12-15 09:46:02 +0900

    Update gcp.tf

--- AWS_GCP_terraform/gcp.tf
+++ AWS_GCP_terraform/gcp.tf
@@ -1,7 +1,7 @@
-provider "google" {
-  credentials = file("gcp_project.json")
+provider "google" { 
+  credentials = file("${path.module}/gcp_project.json")
   project     = "deep-thought-440807-g3"
-  region      = "asia-northeast3"
+  region      = var.gcp_region
 }
 
 resource "google_compute_instance" "bastion_host" {
@@ -13,7 +13,7 @@ resource "google_compute_instance" "bastion_host" {
     initialize_params {
       image = var.gcp_ubuntu_image
       size  = 10
-      type  = "pd-standard"
+      type  = "pd-standard" 
     }
   }
 
@@ -28,7 +28,9 @@ resource "google_compute_instance" "bastion_host" {
   }
 
   metadata = {
-    ssh-keys = "ubuntu:ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEArJQ9...(생략)...key1 ubuntu"
+    ssh-keys = <<EOT
+ubuntu:ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEArJQ9...(생략)...key1 ubuntu
+EOT
   }
 
   tags = ["bastion"]
@@ -37,4 +39,25 @@ resource "google_compute_instance" "bastion_host" {
     email  = var.gcp_service_account_email
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
+}
+
+# 변수 파일 참고 예제
+variable "gcp_region" {
+  default = "asia-northeast3"
+  description = "GCP 리전 설정"
+}
+
+variable "gcp_zone" {
+  default = "asia-northeast3-a"
+  description = "GCP 존 설정"
+}
+
+variable "gcp_ubuntu_image" {
+  default = "projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts"
+  description = "사용할 우분투 이미지"
+}
+
+variable "gcp_service_account_email" {
+  default = "your-service-account@deep-thought-440807-g3.iam.gserviceaccount.com"
+  description = "서비스 계정 이메일"
 }

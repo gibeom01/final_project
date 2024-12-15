b6bd3de1098de2f92b6a6f41be992a647526dcbe
gibeom01 <rlqja7638@gmail.com>
2024-12-15 09:47:48 +0900

    Update gcp.tf

--- AWS_GCP_terraform/gcp.tf
+++ AWS_GCP_terraform/gcp.tf
@@ -40,24 +40,3 @@ EOT
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
-
-# 변수 파일 참고 예제
-variable "gcp_region" {
-  default = "asia-northeast3"
-  description = "GCP 리전 설정"
-}
-
-variable "gcp_zone" {
-  default = "asia-northeast3-a"
-  description = "GCP 존 설정"
-}
-
-variable "gcp_ubuntu_image" {
-  default = "projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts"
-  description = "사용할 우분투 이미지"
-}
-
-variable "gcp_service_account_email" {
-  default = "your-service-account@deep-thought-440807-g3.iam.gserviceaccount.com"
-  description = "서비스 계정 이메일"
-}
